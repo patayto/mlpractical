65e4dad4c4d5c04ae19b17176839556a75d52799
Patrick Laverty <patayto21@gmail.com>
2019-10-29 18:24:25 +0000

    final submission

--- scripts/run_elu_full.sh
+++ scripts/run_elu_full.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python activation_functions_script.py elu 10 100 100 0.001 1
-python activation_functions_script.py elu 10 100 100 0.01 1
-python activation_functions_script.py elu 10 100 100 0.1 1
-python activation_functions_script.py elu 10 100 100 1.0 1
-python activation_functions_script.py elu 10 100 100 10.0 1
+python activation_functions_script.py elu 10 100 100 0.001 0
+python activation_functions_script.py elu 10 100 100 0.01 0
+python activation_functions_script.py elu 10 100 100 0.1 0
+python activation_functions_script.py elu 10 100 100 1.0 0
+python activation_functions_script.py elu 10 100 100 10.0 0
